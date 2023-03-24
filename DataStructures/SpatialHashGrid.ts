0763316af0c03efb84038787ba24647d12cfb37d
Bruno G. A. Lebtag <brlebtag@gmail.com>
2023-03-24 16:15:57 -0300

    Implement optimized Spatial Hash Grid

---
+++ DataStructures/SpatialHashGrid.ts
@@ -0,0 +1,204 @@
+import { Vector2D } from "../../Common";
+import { sat } from '../../Algorithms/Math';
+
+interface Bound {
+    beginX: number;
+    beginY: number;
+    endX: number;
+    endY: number;
+}
+
+interface Dimension {
+    width: number;
+    height: number;
+}
+
+interface Client {
+    position: Vector2D;
+    dimention: Dimension;
+    _queryId: number;
+    _cells: {
+        min: [number, number],
+        max: [number, number],
+        nodes: LinkedList[],
+    }
+}
+
+interface LinkedList {
+    next: LinkedList,
+    previous: LinkedList,
+    client: Client
+}
+
+export default class SpatialHashGrid {
+    private bounds: Bound;
+    private dimentions: Dimension;
+    private cells: LinkedList[][];
+    private queryIds;
+
+    constructor(bounds: Bound, dimentions: Dimension) {
+        this.bounds = bounds; // [[begin1, end1], [begin2, end2]]
+        this.dimentions = dimentions; // [number_quadrants_width, number_quadrants_height]
+        this.cells = [...Array(dimentions.width)].map(_ => [...Array(dimentions.height)].map(_ => (null)));
+        this.queryIds = 0;
+        this.build(); // Lazy Initialization
+    }
+
+    public newClient(position: Vector2D, dimention: Dimension): Client {
+        const client: Client = {
+            position: position,
+            dimention: dimention,
+            _cells: {
+                min: null,
+                max: null,
+                nodes: null,
+            },
+            _queryId: -1,
+        };
+
+        this.insert(client);
+
+        return client;
+    }
+
+    public findNearby(position: Vector2D, bounds: Dimension): Client[] {
+        const { x, y } = position;
+        const { width, height } = bounds;
+
+        const minIndex = this.getCellIndex([x - width / 2, y - height / 2]); 
+        const maxIndex = this.getCellIndex([x + width / 2, y + height / 2]);
+
+        const clients = [];
+        const queryIds = this.queryIds++;
+
+        for (let x = minIndex[0], xn = maxIndex[0]; x <= xn; ++x) {
+            for (let y = minIndex[1], yn = maxIndex[1]; y <= yn; ++y) {
+                let head = this.cells[x][y]
+
+                while (head) {
+                    const v = head.client;
+                    head = head.next;
+                    if (v._queryId != queryIds) {
+                        v._queryId = queryIds;
+                        clients.push(v);
+                    }
+                }
+            }
+        }
+
+        return clients;
+    }
+
+    public UpdateClient(client: Client) {
+        const { x, y } = client.position;
+        const { width, height } = client.dimention;
+
+        const minIndex = this.getCellIndex([x - width / 2, y - height / 2]); 
+        const maxIndex = this.getCellIndex([x + width / 2, y + height / 2]);
+
+        if (client._cells.min[0] == minIndex[0] &&
+            client._cells.min[1] == minIndex[1] && 
+            client._cells.max[0] == maxIndex[0] && 
+            client._cells.max[1] == maxIndex[1]) {
+            return;
+        }
+
+        this.RemoveClient(client);
+        this.insert(client);
+    }
+
+    public RemoveClient(client: Client) {
+        const minIndex = client._cells.min;
+        const maxIndex = client._cells.max;
+
+        for (let x = minIndex[0], xn = maxIndex[0]; x <= xn; ++x) {
+            for (let y = minIndex[1], yn = maxIndex[1]; y <= yn; ++y) {
+                const xi = x - minIndex[0];
+                const yi = y- maxIndex[0];
+                const node = client._cells.nodes[xi][yi];
+
+                if (node.next) {
+                    node.next.previous = node.previous;
+                }
+
+                if (node.previous) {
+                    node.previous.next = node.next;
+                }
+
+                if (!node.previous) {
+                    this.cells[x][y] = node.next;
+                }
+            }
+        }
+
+        client._cells.min = null;
+        client._cells.max = null;
+        client._cells.nodes = null;
+    }
+
+    private insert(client: Client) {
+        const { x, y } = client.position;
+        const { width, height } = client.dimention;
+
+        const minIndex = this.getCellIndex([x - width / 2, y - height / 2]); 
+        const maxIndex = this.getCellIndex([x + width / 2, y + height / 2]);
+        
+        const nodes = [];
+
+        for (let x = minIndex[0], xn = maxIndex[0]; x <= xn; ++x) {
+            nodes.push([]);
+
+            for (let y = minIndex[1], yn = maxIndex[1]; y <= yn; ++y) {
+                const xi = x - minIndex[0];
+
+                const head = {
+                    next: null,
+                    previous: null,
+                    client: client,
+                } as LinkedList;
+
+                nodes[xi].push(head);
+
+                head.next = this.cells[x][y];
+
+                if (this.cells[x][y]) {
+                    this.cells[x][y].previous = head;
+                }
+
+                this.cells[x][y] = head;
+            }
+        }
+
+        client._cells.min = minIndex;
+        client._cells.max = maxIndex;
+        client._cells.nodes = nodes;
+    }
+
+    private build() {
+        const { width, height } = this.dimentions;
+
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                this.cells[this.key(x, y)] = new Set();
+            }
+        }
+    }
+
+    private getCellIndex(position: number[]): [number, number] {
+        const { beginX, endX, beginY, endY } = this.bounds;
+        const { width, height } = this.dimentions;
+
+        const x = sat((position[0] - beginX) / (endX - beginX)); // area_from_begin_to_position / total_area (end - begin)
+        const y = sat((position[1] - beginY) / (endY - beginY));
+
+        // Above, it gives us a percentage. it is the approximate position related to the whole area but in percentage
+        const xIndex = Math.floor(x * (width - 1)); // then, we multiply by number of quadrants we had split the whole area
+        const yIndex = Math.floor(y * (height - 1));
+
+        return [xIndex, yIndex];
+    }
+
+    private key(x: number, y: number) : string {
+        return x + '.' + y;
+    }
+}
