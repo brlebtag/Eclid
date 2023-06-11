72616e105936fc83c263c33bd30dab38a6cba56c
Bruno Lebtag <brlebtag@gmail.com>
2023-06-11 16:12:45 -0300

    Add get begin, end and get index.

--- DataStructures/Lists/CircularArray.ts
+++ DataStructures/Lists/CircularArray.ts
@@ -5,16 +5,16 @@ import { shiftIndex } from "../../Algorithms/Arrays";
 // https://en.wikipedia.org/wiki/Circular_buffer
 
 export default class CircularArray<T> {
-    private storage: T[];
+    private _storage: T[];
     private _capacity: number;
-    private begin: number; // points to the first element in the circular array
-    private end: number; // points to the next available (free) spot
-    private total: number; // total elements
+    private _begin: number; // points to the first element in the circular array
+    private _end: number; // points to the next available (free) spot
+    private _total: number; // total elements
 
     constructor(capacity: number, elements: any[] = null) {
-        this.storage = new Array(capacity);
+        this._storage = new Array(capacity);
         this._capacity = capacity;
-        this.begin = this.end = this.total = 0;
+        this._begin = this._end = this._total = 0;
 
         if (elements) {
             if (elements.length > capacity) {
@@ -22,93 +22,97 @@ export default class CircularArray<T> {
             }
 
             let len = elements.length;
-            const storage = this.storage;
+            const storage = this._storage;
             for (let i = 0; i < len; i++) {
                 storage[i] = elements[i];
             }
-            this.total = elements.length;
-            this.end = elements.length % this._capacity;
+            this._total = elements.length;
+            this._end = elements.length % this._capacity;
         }
     }
 
     clear(): void {
-        this.total = this.begin = this.end = 0;
+        this._total = this._begin = this._end = 0;
     }
 
     front(): T {
-        if (this.total === 0) return undefined; // empty
-        return this.storage[this.begin];
+        if (this._total === 0) return undefined; // empty
+        return this._storage[this._begin];
     }
 
     back(): T {
-        if (this.total === 0) return undefined; // empty
-        return this.storage[this.end];
+        if (this._total === 0) return undefined; // empty
+        return this._storage[this._end];
     }
 
     // insert at the end
     push(value: T): void {
-        const _capacity = this._capacity;
-        const end = this.end;
-        this.storage[end] = value;
-        this.end = (end + 1) % _capacity;
-        this.total++;
-        if (this.total >= _capacity) {
-            this.begin = (this.begin + 1) % _capacity;
-            this.total = _capacity;
+        const capacity = this._capacity;
+        const end = this._end;
+        this._storage[end] = value;
+        this._end = (end + 1) % capacity;
+        this._total++;
+        if (this._total >= capacity) {
+            this._begin = (this._begin + 1) % capacity;
+            this._total = capacity;
         }
     }
 
     // insert at the beginning
     unshift(value: T): void {
-        const _capacity = this._capacity;
-        this.begin--;
-        if (this.begin < 0) {
-            this.begin = this._capacity - 1; // go to the end
+        const capacity = this._capacity;
+        this._begin--;
+        if (this._begin < 0) {
+            this._begin = this._capacity - 1; // go to the end
         }
-        this.storage[this.begin] = value;
-        this.total++;
-        if (this.total >= _capacity) {
-            this.end--;
-            this.total = _capacity;
-            if (this.end < 0) {
-                this.end = _capacity - 1;
+        this._storage[this._begin] = value;
+        this._total++;
+        if (this._total >= capacity) {
+            this._end--;
+            this._total = capacity;
+            if (this._end < 0) {
+                this._end = capacity - 1;
             }
         }
     }
     
     // remove at the end
     pop(): T {
-        const _capacity = this._capacity;
-        this.end--;
-        if (this.end < 0) {
-            this.end = _capacity - 1;
+        const capacity = this._capacity;
+        this._end--;
+        if (this._end < 0) {
+            this._end = capacity - 1;
         }
-        let el = this.storage[this.end];
-        this.storage[this.end] = undefined; // empty the slot
-        this.total--;
+        let el = this._storage[this._end];
+        this._storage[this._end] = undefined; // empty the slot
+        this._total--;
         return el;
     }
 
     // remove at the beginning
     shift(): T {
-        const _capacity = this._capacity;
-        let el = this.storage[this.begin];
-        this.storage[this.begin] = undefined; // empty the slot
-        this.begin = (this.begin + 1) % _capacity;
-        this.total--;
+        const capacity = this._capacity;
+        let el = this._storage[this._begin];
+        this._storage[this._begin] = undefined; // empty the slot
+        this._begin = (this._begin + 1) % capacity;
+        this._total--;
         return el;
     }
 
     at(index: number): T {
         const capacity = this._capacity;
-        index += this.begin;
-        if (index >= 0) return this.storage[index % capacity]; // shift right
+        index += this._begin;
+        if (index >= 0) return this._storage[index % capacity]; // shift right
         let newIndex = capacity - ((-index) % capacity); // shift to left
-        return this.storage[newIndex === capacity ? 0 : newIndex];
+        return this._storage[newIndex === capacity ? 0 : newIndex];
+    }
+
+    get(index: number): T {
+        return this._storage[index];
     }
 
     get length(): number {
-        return this.total;
+        return this._total;
     }
     
     get capacity(): number {
@@ -116,10 +120,18 @@ export default class CircularArray<T> {
     }
 
     full(): boolean {
-        return this.total === this._capacity;
+        return this._total === this._capacity;
     }
 
     empty() {
-        return this.total === 0;
+        return this._total === 0;
+    }
+
+    get begin() {
+        return this._begin;
+    }
+    
+    get end() {
+        return this._end;
     }
 }
